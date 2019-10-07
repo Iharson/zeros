061e10f39f4507815efb3ce7bfdd52a87701ba13
Iharson <mashniakou@gmail.com>
2019-10-07 19:41:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    let arr = expression.split('*');
+    let fives = 0;
+    let twos = 0;
+
+    arr.forEach(element => {
+
+        let match = element.match(/!/g).length;
+        element = parseInt(element);
+
+        for (element; element > 0; element -= match) {
+            let e = element;
+            while (e % 5 === 0) {
+                fives++;
+                e /= 5;
+            }
+
+            while (e % 2 === 0) {
+                twos++;
+                e /= 2;
+            }
+        }
+    });
+
+    return fives > twos ? twos : fives;
+};
